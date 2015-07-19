4ccae76f962050bc157638682b240c6c28d19eff
dinnozap <dinnozap@gmail.com>
2015-07-19 22:11:04 +0200

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -17,13 +17,8 @@ JAUNE="\\033[1;33m"
 CYAN="\\033[1;36m"
 clear
 cd Spigot
-echo -e Lancement du serveur... $NORMAL
-echo -e $BLEU Pour acceder a la console : screen -r $NORMAL
+echo -e "Lancement du serveur... $NORMAL"
+echo -e "L'IP de votre serveur est... $BLEU"
+curl ifconfig.me
+echo -e $NORMAL Pour acceder a la console : screen -r $NORMAL
 screen -dmS minecraft java -Xms1024M -Xmx1024M -jar spigot.jar nogui
-read -p 'Voulez vous configurer vos plugins ? oui/non' ouinon
-if [ ouinon = "oui" ]
-then
-	cd ..
-	./Plugins.sh
-
-fi
