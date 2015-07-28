c44bffc7c288dbe28a4016da3cd3d7fd50d4a87f
dinnozap <dinnozap@gmail.com>
2015-07-28 22:42:38 +0200

    Update Launcher.sh

--- Launcher.sh
+++ Launcher.sh
@@ -3,9 +3,9 @@
 #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 #||||||||||||||||||||McServerMaker||||||||||||||||||||||||||||||||||||||
 #|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-#_______________________________________________________________________
-
+#________________________________________________________________________
 #Couleurs
+
 VERT="\\033[1;32m" 
 NORMAL="\\033[0;39m" 
 ROUGE="\\033[1;31m" 
@@ -17,27 +17,27 @@ JAUNE="\\033[1;33m"
 CYAN="\\033[1;36m"
 chmod 777 StartServer.sh
 chmod 777 ServerMaker.sh
-chmod 777 Plugins.sh
-echo -e $BLEU '
-________________________________________________________________________
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-||||||||||||||||||||||||||||||McServerMaker|||||||||||||||||||||||||||||
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-________________________________________________________________________
-
-
-
-'
-read -p 'Souhaitez-vous:' choix
-1.Créer votre serveur
-2.Installer des Plugins sur votre serveur
-
-if [ $choix == "2" ]
+chmod 777 PluginUtils.sh
+read -p 'Que voulez vous faire ? MakeServer/StartServer/Plugins/Extentions : ' faire
+if [ $faire == "Plugins" ]
 then
-	sh Plugins.sh
-elif [ $choix == "1" ]
+	sh PluginUtils.sh
+elif [ $faire == "StartServer" ]
+then
+	sh StartServer.sh
+elif [ $faire == "MakeServer" ]
 then
 	sh ServerMaker.sh
+elif [ $faire == "Extentions" ]
+then
+	cd extentions
+	clear
+	echo -e "$ROUGE/!\ $BLEU si il n'est rien écrit c'est que vous n'avez pas encore installés d'extentions $NORMAL $ROUGE/!\ "
+	echo -e "$BLEU Extentions disponibles  :$NORMAL"
+	ls
+	echo -e "$ROUGE/!\ $BLEU écrivez le nom de l'extention avec exactitude sans saisire le .sh "
+	read -p 'Quelle extention choisisez vous ? : ' plugins
+	sh "$plugins".sh
 else
-	echo -e $BLEU 'Commande inconnue, réessayez en réecrivant la commande avec exactitude'$NORMAL
+echo -e $BLEU Commande inconnue, réessayez en réecrivant la commande avec exactitude$NORMAL
 fi
